70aae8f240d9fa2e561b8b67f45cfd7467b5246c
Алексей Небензя <anebenzya@initium.ru>
2022-10-25 23:04:56 +0300

    upd class work

--- Fraction/Fraction.cpp
+++ Fraction/Fraction.cpp
@@ -1,138 +1,189 @@
-﻿#pragma once
-#include <iostream>
+﻿#include<iostream>
 using namespace std;
-
-int* Push_front(int* arr, int& size, int value)
-{
-	int* bufArr = new int[++size]{};
-	bufArr[0] = value;
-
-	for (int i = 1; i < size; i++)
-	{
-		bufArr[i] = arr[i - 1];
-	}
-	delete[] arr;
-	return bufArr;
-}
-
-
 
 class Fraction
 {
-	int _integer = 0, _numerator = 0, _denominator = 1;
-
-	char _IntToChar(int value) {
-		return value + 48;
-	}
-
-	void PrintInt(int value) {
-
-		if (value >= 10) {
-
-			int size = 0;
-			int* buf = new int[size];
-			while (value)
-			{
-				buf = Push_front(buf, size, value % 10);
-				value /= 10;
-			}
-
-			for (int i = 0; i < size; i++)
-			{
-				cout << _IntToChar(buf[i]);
-			}
-			delete[]buf;
-		}
-		else if (value >= 0)
-			cout << _IntToChar(value);
-	}
-
-	void _CalculateInteger() {
-
-		if (_numerator >= _denominator) {
-			_integer += _numerator / _denominator;
-
-			if (_numerator == _denominator) {
-				_numerator = 0; _denominator = 1;
-			}
-			else
-				_numerator = _numerator % _denominator;
-		}
-	}
+	int integer, numerator, denominator;
 
 public:
+#pragma region Get/Set
 
-	Fraction(int numerator = 0, int denominator = 1) {
-		setFractionalPart(numerator, denominator);
-		_CalculateInteger();
+	int getInteger()const
+	{
+		return integer;
+	}
+	int getNumerator()const
+	{
+		return numerator;
+	}
+	int getDenominator()const
+	{
+		return denominator;
+	}
+	void setInteger(int integer)
+	{
+		this->integer = integer;
+	}
+	void setNumerator(int numerator)
+	{
+		this->numerator = numerator;
+	}
+	void setDenominator(int denominator)
+	{
+		if (denominator == 0)denominator = 1;
+		this->denominator = denominator;
 	}
 
-	Fraction(int integer, int numerator, int denominator) {
-		setIntegerPart(integer);
-		setFractionalPart(numerator, denominator);
-		_CalculateInteger();
+#pragma endregion
+
+#pragma region Constructor
+
+	Fraction()
+	{
+		this->integer = 0;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "DefConstructor:\t" << this << endl;
+	}
+	Fraction(int integer)
+	{
+		setInteger(integer);
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "1ArgConstructor:" << this << endl;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->integer = 0;
+		setNumerator(numerator);
+		setDenominator(denominator);
+		cout << "Constructor:\t" << this << endl;
+	}
+	Fraction(int integer, int numerator, int denominator)
+	{
+		setInteger(integer);
+		setNumerator(numerator);
+		setDenominator(denominator);
+		cout << "Constructor:\t" << this << endl;
+	}
+	Fraction(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyConstructor:" << this << endl;
+	}
+	~Fraction()
+	{
+		cout << "Destructor:\t" << this << endl;
 	}
 
-	void setIntegerPart(int value) {
-		_integer = value;
-	}
+#pragma endregion
 
-	void setFractionalPart(int numerator, int denominator) {
+#pragma region OperatorsOld
 
-		if (numerator > 0 && denominator > 0)
-		{
-			_numerator = numerator;
-			_denominator = denominator;
-			_CalculateInteger();
-		}
-	}
-
-	int getInteger() const { return _integer; }
-	int getNumerator() const { return _numerator; }
-	int getDenominator() const { return _denominator; }
-
-	Fraction& operator=(const Fraction& other) {
-		this->_integer = other._integer;
-		this->_numerator = other._numerator;
-		this->_denominator = other._denominator;
+	Fraction& operator=(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyAssignment:\t" << this << endl;
 		return *this;
 	}
 
-	void Print() {
-		if (_integer != 0) {
-			PrintInt(getInteger());
-			cout << " ";
-		}
-
-		if (_numerator != 0) {
-			PrintInt(getNumerator());
-			cout << "/";
-			PrintInt(getDenominator());
-		}
-
-		cout << endl;
+	Fraction& operator++()
+	{
+		integer++;
+		return *this;
 	}
+	Fraction operator++(int)
+	{
+		Fraction old = *this;
+		integer++;
+		return old;
+	}
+
+#pragma endregion
+
+#pragma region FunctionsOld
+
+	Fraction& ToImproper()
+	{
+		numerator += integer * denominator;
+		integer = 0;
+		return *this;
+	}
+	Fraction& ToProper()
+	{
+		integer = numerator / denominator;
+		numerator %= denominator;
+		return *this;
+	}
+	Fraction Inverted()const
+	{
+		Fraction Inverted = *this;
+		Inverted.ToImproper();
+		swap(Inverted.numerator, Inverted.denominator);
+		return Inverted;
+	}
+#pragma endregion
+
 };
 
-Fraction operator+(const Fraction& left, const Fraction& right) {
-	Fraction result;
-	result.setIntegerPart(left.getInteger() + right.getInteger());
+#pragma region operatorsOld
 
-	if (left.getDenominator() == right.getDenominator())
+ostream& operator<<(ostream& os, const Fraction& obj)
+{
+	if (obj.getInteger())os << obj.getInteger();
+	if (obj.getNumerator())
 	{
-		result.setFractionalPart(left.getNumerator() + right.getNumerator(), left.getDenominator());
+		if (obj.getInteger())os << "(";
+		os << obj.getNumerator() << "/" << obj.getDenominator();
+		if (obj.getInteger())os << ")";
 	}
-
-	return result;
+	else if (obj.getInteger() == 0) os << 0;
+	return os;
+}
+Fraction operator*(Fraction left, Fraction right)
+{
+	left.ToImproper();
+	right.ToImproper();
+	return Fraction
+	(
+		left.getNumerator() * right.getNumerator(),
+		left.getDenominator() * right.getDenominator()
+	).ToProper();
+}
+Fraction operator/(const Fraction& left, const Fraction& right)
+{
+	return left * right.Inverted();
+}
+Fraction operator+(Fraction left, Fraction right)
+{
+	left.ToImproper();
+	right.ToImproper();
+	return Fraction
+	(
+		left.getNumerator() * right.getDenominator() + right.getNumerator() * left.getDenominator(),
+		left.getDenominator() * right.getDenominator()
+	).ToProper();
 }
 
+#pragma endregion
 
-int main()
+
+void main()
 {
-	Fraction A(12, 2, 5);
-	Fraction B(4, 5);
-	Fraction C = A + B;
-	A.Print();
-	B.Print();
-	C.Print();
+	Fraction A(1, 2, 3);
+	Fraction B(3, 4, 5);
+	Fraction C = A * B;
+	cout << C << endl;
+	cout << A / B << endl;
+	cout << A + B << endl;
+
+	for (Fraction i(3, 4); i.getInteger() < 10; ++i)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;
 }
