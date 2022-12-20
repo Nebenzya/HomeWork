a056d7feeef8234d193c63f52685ca25fe4462e5
Pechenuyha <aleksei6191@gmail.com>
2022-12-20 17:07:22 +0300

    27.12.2022

---
+++ STL/STL.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <list>
+
+int main()
+{
+    std::list<int> list = { 3,5,8,13,21,34 };
+    std::list<int>::iterator it = list.begin();
+    int n,i;
+
+    std::cout << "Enter number: "; std::cin >> n;
+    do
+    {
+        std::cout << "Enter index: "; std::cin >> i;
+    } while (i > list.size());
+
+    
+    list.insert(it,n);
+    //list.insert(list.begin() + i, n); WHY NOT?
+    
+    for (int i : list) 
+    {
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
+}
