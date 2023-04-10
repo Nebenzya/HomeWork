b5f01cda0dced75b53dca65def60af3411d0a557
Алексей Небензя <aleksei6191@gmail.com>
2023-04-10 21:07:11 +0300

    9.04.2023

---
+++ DisplaySize/Program.cs
@@ -0,0 +1,7 @@
+﻿//Разработать приложение, позволяющее определить
+//размер диагонали монитора текущего компьютера в дюймах
+
+using System.Windows.Devices.Display;
+
+DisplayMonitor dm = new();
+Console.WriteLine(dm.PhysicalSizeInInches);
