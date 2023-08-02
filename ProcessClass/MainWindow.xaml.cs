4679533bef7356f4693e5708969e6712f5415b0f
Алексей Небензя <aleksei6191@gmail.com>
2023-08-02 17:58:25 +0300

    23.07.2023

---
+++ ProcessClass/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace ProcessClass;
+    
+public partial class MainWindow : Window
+{
+    private Process _process;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _process = new();
+        _process.StartInfo = new ProcessStartInfo("notepad.exe");
+        _process.Exited += Process_Exited;
+    }
+
+    private void Process_Exited(object? sender, EventArgs e)
+    {
+        inputText.Text = string.Empty;
+    }
+
+    private void start_Click(object sender, RoutedEventArgs e)
+    {
+        _process.StartInfo.Arguments = inputText.Text;
+        _process.Start();
+    }
+}
